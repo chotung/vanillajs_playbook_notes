f640cc52a06f15607c65b483e31527497fc01177
chotung <38818833+chotung@users.noreply.github.com>
2021-06-02 13:11:51 -0400

    hide and show input

--- src/index.js
+++ src/index.js
@@ -89,92 +89,15 @@ function addEditBtnEvent() {
 function editEvent(e) {
 	const noteGroup = document.querySelector(".note_group");
 	const noteId = e.target.parentElement.parentElement.dataset.id;
-	const textAreas = document.querySelectorAll(".text_area");
-	const textDivs = document.querySelectorAll(".text_div");
-	// do I have a current note
-	// 	if not set it to the current note
-	// now i have a current Post means I'm looking at a post
-	//  what if I click another edit button
-	//  	check if the edit button i clicke has the same id as the curr post
-	//  		if so toggle
-	if (!state.currPost) {
-		state.currPost = noteId;
-	} else {
-		if (state.currPost === noteId) {
-			// console.log("clickgin same post");
-			state.notes.find((note) => {
-				if (note.id === noteId) {
-					if (note.clicked.length < 1) {
-						const toggledElements = [];
-
-						for (let txtArea of textAreas) {
-							const clickedEditArea = txtArea.parentElement.parentElement;
-							const clickedNote = e.target.parentElement.parentElement;
-							if (clickedEditArea.dataset.id === clickedNote.dataset.id) {
-								toggledElements.push(txtArea);
-							}
-						}
-
-						for (let txtDiv of textDivs) {
-							const clickedEditArea = txtDiv.parentElement.parentElement;
-							const clickedNote = e.target.parentElement.parentElement;
-							if (clickedEditArea.dataset.id === clickedNote.dataset.id) {
-								toggledElements.push(txtDiv);
-							}
-						}
-
-						console.log(toggledElements);
-						toggleTextAreaAndDiv(toggledElements);
-
-						note.clicked.push(note);
-					} else if (note.clicked.length === 1) {
-						note.text = textArea.value;
-						note.clicked.pop(note);
-						// toggleTextAreaAndDiv(textArea, textDiv);
-						state.currPost = "";
-					}
-				}
-			});
-		}
-	}
-	// console.log(state.currPost, noteId);
-
-	// state.notes.find((note) => {
-	// 	if (note.id === noteId) {
-	// 		if (note.clicked.length < 1) {
-	// 			toggleTextAreaAndDiv(textArea, textDiv);
-	// 			note.clicked.push(note);
-	// 		} else if (note.clicked.length === 1) {
-	// 			note.text = textArea.value;
-	// 			note.clicked.pop(note);
-	// 			toggleTextAreaAndDiv(textArea, textDiv);
-	// 			resetDom(noteGroup);
-	// 		}
-	// 	}
-	// });
+	const textBody = e.target.parentElement.nextElementSibling;
+	const textDiv = textBody.querySelector(".text_div");
+	const textArea = textBody.querySelector(".text_area");
+	toggleInputArea(textDiv, textArea);
 }
 
-function getTextAreaAndDiv(textAreas, textDivs) {}
-
-function toggleTextAreaAndDiv(toggledElements) {
-	const [textArea, textDiv] = toggledElements;
-	console.log(textArea.classList, textDiv.classList);
+function toggleInputArea(textDiv, textArea) {
 	textArea.classList.toggle("hide");
 	textDiv.classList.toggle("hide");
-	// console.dir(textArea, textDiv);
-	// textArea.forEach((text) => {
-	// 	console.log(text, id);
-
-	// if (text.dataset.id === id) {
-	// console.dir(text);
-	// text.classList.toggle("hide");
-	// }
-	// });
-	// textDiv.forEach((textDiv) => {
-	// 	if (textDiv.dataset.id === id) {
-	// 		textDiv.classList.toggle("hide");
-	// 	}
-	// });
 }
 
 function addDeleteBtnEvent() {
