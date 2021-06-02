ad251ef1a9e7293dd8228ed32abb7200b6645c7c
chotung <38818833+chotung@users.noreply.github.com>
2021-06-02 16:26:58 -0400

    Update Note.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 class Note {
 	constructor(color, btnColor) {
 		this.id = uuidv4();
-		this.text = "some random text";
+		this.text = "";
 		this.color = color;
 		this.btnColor = btnColor;
 		this.clicked = false;
